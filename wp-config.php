1d417fade585f18df1ef799c2251a9d9680c819c
Chris He Li <helisz101@gmail.com>
2015-06-05 15:11:00 +0800

    Config

--- wp-config.php
+++ wp-config.php
@@ -19,16 +19,16 @@
 define('DB_NAME', 'chrishe');
 
 /** MySQL database username */
-define('DB_USER', 'chrishe');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '8959865');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+/** Database Charset to use in creating database tables. */ //utf8mb4
+define('DB_CHARSET', '');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
